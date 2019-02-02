5cc536ae20b21164f12b412dcf57971bca02de70
Jahn <ioannis.kiwe@gmail.com>
2019-02-02 12:17:17 +0100

    postgresql-sink/src/main/scala/jahnestacado/postgresql/sink/rdbms/ConnectionPool.scala: Improved error handling

--- postgresql-sink/src/main/scala/jahnestacado/postgresql/sink/rdbms/ConnectionPool.scala
+++ postgresql-sink/src/main/scala/jahnestacado/postgresql/sink/rdbms/ConnectionPool.scala
@@ -6,32 +6,42 @@ import com.typesafe.scalalogging.LazyLogging
 import jahnestacado.postgresql.sink.Config
 import org.postgresql.jdbc3.Jdbc3PoolingDataSource
 
+import scala.util.{Failure, Success, Try}
+
 class ConnectionPool(config: Config) extends LazyLogging {
   private var connection: Option[Connection] = None
 
-  private def initializeConnection(): Option[Connection] = {
-    val source = new Jdbc3PoolingDataSource()
-    source.setServerName(config.postgresql.host)
-    source.setPortNumber(config.postgresql.port)
-    source.setDatabaseName(config.postgresql.database)
-    source.setUser(config.postgresql.user)
-    source.setPassword(config.postgresql.password)
-    source.setMaxConnections(config.postgresql.maxConnections)
-    logger.info(s"Established connection with postgres instance ${config.postgresql.host}:${config.postgresql.port}")
-    Some(source.getConnection())
+  private def initializeConnection(): Try[Connection] = {
+    Try {
+      val source = new Jdbc3PoolingDataSource()
+      source.setServerName(config.postgresql.host)
+      source.setPortNumber(config.postgresql.port)
+      source.setDatabaseName(config.postgresql.database)
+      source.setUser(config.postgresql.user)
+      source.setPassword(config.postgresql.password)
+      source.setMaxConnections(config.postgresql.maxConnections)
+      source.getConnection()
+    }
   }
 
   def get(): Option[Connection] = {
     if (connection.isEmpty) {
-      connection = initializeConnection()
+      initializeConnection() match {
+        case Success(conn) =>
+          logger.info(s"Established connection with postgres instance" +
+            s" ${config.postgresql.host}:${config.postgresql.port}")
+          connection = Some(conn)
+        case Failure(ex) =>
+          logger.error(ex.getMessage)
+      }
+
       connection
     } else {
-      try {
-        connection.get.getClientInfo()
-        connection
-      } catch {
-        case ex: Exception =>
-          logger.error(ex.getMessage())
+      Try(connection.get.getClientInfo()) match {
+        case Success(_) =>
+          connection
+        case Failure(ex) =>
+          logger.error(ex.getMessage)
           connection.get.close()
           connection = None
           connection
