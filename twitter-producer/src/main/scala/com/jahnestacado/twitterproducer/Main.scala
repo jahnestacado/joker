f07707ec4aa60b05c2d883ef6f51d2a74d81ddf9
Jahn <ioannis.kiwe@gmail.com>
2019-02-03 22:09:01 +0100

    Improved error handling when schema-registry is not accessible

--- twitter-producer/src/main/scala/com/jahnestacado/twitterproducer/Main.scala
+++ twitter-producer/src/main/scala/com/jahnestacado/twitterproducer/Main.scala
@@ -1,23 +1,18 @@
 package com.jahnestacado.twitterproducer
 
-import ch.qos.logback.classic.{Level, Logger}
 import com.danielasfregola.twitter4s.TwitterStreamingClient
 import com.danielasfregola.twitter4s.entities.Tweet
 import com.typesafe.scalalogging.LazyLogging
-import org.slf4j.LoggerFactory
 
 object Main extends App with LazyLogging {
-  LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME).asInstanceOf[Logger].setLevel(Level.TRACE)
 
   val config: Config = new Config()
   val client = TwitterStreamingClient()
   val tracks = config.twitter.tracks
-  println("tracks", tracks)
+
   val producer = new TweetKafkaProducer(config)
-  val t0 = System.currentTimeMillis()
   client.filterStatuses(tracks = tracks, stall_warnings = true) {
     case tweet: Tweet => {
-      println(s"Received tweet-- ${System.currentTimeMillis() - t0}")
       producer.send(tweet)
     }
   }
--- twitter-producer/src/main/scala/com/jahnestacado/twitterproducer/TweetKafkaProducer.scala
+++ twitter-producer/src/main/scala/com/jahnestacado/twitterproducer/TweetKafkaProducer.scala
@@ -4,40 +4,36 @@ import java.util.Properties
 
 import com.danielasfregola.twitter4s.entities.{Tweet => TweetOrig}
 import com.jahnestacado.model.Tweet
+import com.typesafe.scalalogging.LazyLogging
 import org.apache.kafka.clients.producer._
 import org.apache.kafka.common.serialization.StringSerializer
 
-class TweetKafkaProducer(config: Config) {
+import scala.util.{Failure, Try}
+
+class TweetKafkaProducer(config: Config) extends LazyLogging {
   private val shemaRegistryUrl: String = config.kafkaProducer.schemaRegistryUrl
   private val topicName: String = config.kafkaProducer.topic
   private val boostrapServers: String = config.kafkaProducer.bootstrapServers
-  private val props = new Properties()
 
+  private val props = new Properties()
   props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, boostrapServers)
   props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer].getName)
   props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[io.confluent.kafka.serializers.KafkaAvroSerializer].getName)
   props.put(ProducerConfig.ACKS_CONFIG, config.kafkaProducer.acks)
   props.put("schema.registry.url", shemaRegistryUrl)
 
-  private val producer = new KafkaProducer[String, Tweet](props)
-
-  class OnDone extends Callback {
-
-    override def onCompletion(metadata: RecordMetadata, exception: Exception): Unit = {
-      if (exception != null) {
-        println(exception)
-      } else {
-        println(s"onCompletion - ${metadata}")
-      }
-    }
-  }
+  private val producer: KafkaProducer[String, Tweet] = new KafkaProducer[String, Tweet](props)
 
   def send(tweet: TweetOrig): Unit = {
     val avroTweet = TweetToAvroMapper.mapTweet(tweet)
     val record: ProducerRecord[String, Tweet] = new ProducerRecord(topicName, avroTweet)
-      producer.send(record, new OnDone)
+    logger.debug(s"Sending record ${record.value()}")
+    Try(producer.send(record)) match {
+      case Failure(ex) =>
+        logger.error(s"An error occured while sending record. ${ex.getMessage}")
+    }
   }
-
-
 }
+
+
 
