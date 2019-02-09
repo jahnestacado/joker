40bc52473f51640769b1978bef530c0d8ed42127
Jahn <ioannis.kiwe@gmail.com>
2019-02-10 00:10:37 +0100

    postgresql-sink/src/main/scala/jahnestacado/postgresql/sink/kafka/Consumer.scala: Improved flow and added supervision strategy

--- postgresql-sink/src/main/scala/jahnestacado/postgresql/sink/kafka/Consumer.scala
+++ postgresql-sink/src/main/scala/jahnestacado/postgresql/sink/kafka/Consumer.scala
@@ -1,9 +1,10 @@
 package jahnestacado.postgresql.sink.kafka
 
-import akka.kafka.ConsumerMessage.CommittableOffsetBatch
 import akka.kafka.scaladsl.{Consumer => KafkaConsumer}
 import akka.kafka.{ConsumerSettings, Subscriptions}
+import akka.stream.ActorAttributes.supervisionStrategy
 import akka.stream.Materializer
+import akka.stream.Supervision.resumingDecider
 import akka.stream.scaladsl.{RestartSource, Sink}
 import com.typesafe.scalalogging.LazyLogging
 import jahnestacado.postgresql.sink.Config
@@ -15,27 +16,24 @@ import scala.concurrent.duration._
 class Consumer[T](connectionPool: ConnectionPool, config: Config, Persistor: Persistor[T])
                  (implicit executionContext: ExecutionContext, materializer: Materializer) extends LazyLogging{
 
-  val consumerSettings: ConsumerSettings[String, T] = config.kafkaConsumer.getConsumerSettings[T]
-  val restartSource = RestartSource.onFailuresWithBackoff(
+  private val parallelism: Int = 2
+  private val consumerSettings: ConsumerSettings[String, T] = config.kafkaConsumer.getConsumerSettings[T]
+  private val restartSource = RestartSource.onFailuresWithBackoff(
     minBackoff = 30.seconds,
     maxBackoff = 2.minutes,
     randomFactor = 0.4
   ) { () =>
-    KafkaConsumer.committableSource(consumerSettings, Subscriptions.topics(Persistor.topic))
-      .mapAsync(2) { msg =>
-        var connection = connectionPool.get()
-        logger.debug(s"Received record ${msg.record.value()}")
-        Persistor.insert(connection.get, msg.record.value())
-          .map(_ => {
-            msg.committableOffset
-          })
-      }
-      .batch(max = 10, first => CommittableOffsetBatch.empty.updated(first)) { (batch, elem) =>
-        batch.updated(elem)
-      }
-
-      .mapAsync(2)(_.commitScaladsl())
+    KafkaConsumer.committablePartitionedSource(consumerSettings, Subscriptions.topics(Persistor.topic))
+      .flatMapMerge(parallelism, _._2)
   }
-
+    .mapAsync(parallelism) { msg =>
+      var connection = connectionPool.get()
+      logger.debug(s"Received record ${msg.record.value()}")
+      Persistor
+        .insert(connection.get, msg.record.value())
+        .map(_ => msg.committableOffset)
+    }
+    .mapAsyncUnordered(parallelism)(_.commitScaladsl())
+    .withAttributes(supervisionStrategy(resumingDecider))
     .runWith(Sink.ignore)
 }
