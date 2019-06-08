5ab846d0e4d37caefca1aa63e27bd89e5cc52e60
Jahn <ioannis.kiwe@gmail.com>
2019-06-08 10:54:04 +0200

    twitter-producer/src/main/scala/com/jahnestacado/twitterproducer/Main.scala: Service noew terminates upon stream errors to prevent zombie state

--- twitter-producer/src/main/scala/com/jahnestacado/twitterproducer/Main.scala
+++ twitter-producer/src/main/scala/com/jahnestacado/twitterproducer/Main.scala
@@ -5,8 +5,11 @@ import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.SourceQueueWithComplete
 import com.danielasfregola.twitter4s.TwitterStreamingClient
 import com.danielasfregola.twitter4s.entities.{Tweet => RawTweet}
+import com.danielasfregola.twitter4s.http.clients.streaming.TwitterStream
 import com.jahnestacado.twitterproducer.kafka.ProducerSourceQueue
 import com.typesafe.scalalogging.LazyLogging
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
 
 object Main extends App with LazyLogging {
 
@@ -14,15 +17,27 @@ object Main extends App with LazyLogging {
     val config: Config = new Config()
     implicit val system = ActorSystem("twitter-producer")
     implicit val mat = ActorMaterializer()
+    implicit val context = system.dispatcher
+
     val producerSourceQueue: SourceQueueWithComplete[RawTweet] = new ProducerSourceQueue(config).run()
     val keywords: List[String] = config.twitter.keywords
 
     val client: TwitterStreamingClient = TwitterStreamingClient()
-    client.filterStatuses(tracks = keywords, stall_warnings = true)({
+    val twitterStreamFuture: Future[TwitterStream] = client.filterStatuses(tracks = keywords, stall_warnings = true)({
       case tweet: RawTweet =>
         producerSourceQueue.offer(tweet)
     }, {
-      case ex: Throwable => logger.error(ex.getMessage)
+      case ex: Throwable =>
+        logger.error(ex.toString)
+        system.terminate()
+    })
+
+
+    twitterStreamFuture.onComplete({
+      case Success(_) => logger.info("")
+      case Failure(ex) =>
+        logger.error(ex.toString)
+        system.terminate()
     })
   }
 
